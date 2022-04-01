043b6f967dc1b4fc4784acabe124377f1e10442a
Marcas Anderson <58535201+marcas77@users.noreply.github.com>
2022-04-01 09:53:09 -0400

    Add files via upload

--- Cayley Table.py
+++ Cayley Table.py
@@ -6,27 +6,31 @@
 
 import pandas as p
 
-def tableprinter(newU,mod,xlname):
-    "Def declares a final list which which computes what values should be in the table, then prints them."
-    
-    "Creates the list we enter into the dataframe at the end to make the xlsx values"
+def Zprinter(newZ,mod,xlname):
+    biglist = []
+    for i in range(0,len(newZ)):
+        list3 = []
+        for k in range(0,len(newZ)):
+            if (k==0):
+                list3.append(newZ[i])
+            else:
+                list3.append((newZ[k]+newZ[i]) % mod)
+        print(list3)
+        biglist.append(list3)
+    df = p.DataFrame(biglist,index=newZ,columns=newZ)
+    df.to_excel(xlname,freeze_panes=[1,1])
+    return
+
+
+def Uprinter(newU,mod,xlname):
     biglist = []
     for i in range(0,len(newU)):
         list3 = []
         for k in range(0,len(newU)):
             if (k==0):
-                # string=str(newU[i])
-                # while(len(string)!=largest):
-                #     string+=" "
-                
-                # list3.append(string)
                 list3.append(newU[i])
             else:
-                x=newU[k]*newU[i]
-                if(x > newU[len(newU)-1]):
-                    list3.append((newU[k]*newU[i]) % mod)
-                else:
-                    list3.append((newU[k]*newU[i]))
+                list3.append((newU[k]*newU[i]) % mod)
         print(list3)
         biglist.append(list3)
     "Creates the dataframe we use to hold the biglist"
@@ -35,42 +39,73 @@ def tableprinter(newU,mod,xlname):
     df.to_excel(xlname,freeze_panes=[1,1])
     return
 
+
 def Compare(U,mod):
     "Def declares a new list, then compares lists U and mod for any factors from mod (excluding 1)."
     "Function will add all values from U into list2, excluding those factors of mod."
-    
-    list2 = [1]
+    list2 = U.copy()
     for i in range(1,len(U)):
         for k in range(1,len(mod)):
-            if(U[i] % mod[k] != 0):
-                list2.append(U[i])
-            break
+            if(U[i] % mod[k] == 0):
+                print("Found ",U[i]," mod ",mod[k],"= 0. Removing from the list.")
+                list2.remove(U[i])
+                break
+    #print(list2,"endlist")
     return list2
-
-def declist(U):
-    "Def declares a list with numbers 1 through U for the U list."
-    
-    list0 = []
-    for i in range(1,U+1):
-        list0.append(i)
-    return list0
 
 
 def ModFactors(x):
     "Def declares a list and fills list with factors of input integer."
-    
     list1 = []
     for i in range(1,x+1):
         if(x % i == 0):
             list1.append(i)
+    #print(list1,"ModFactors")
     return list1
 
-def main(U):
-    "Creates the file name for the Cayley Table"
+
+def declist(U,AddClause):#
+    "Def declares a list with numbers 1 through U for the U list."
+    list0 = []
+    if(AddClause == 1):
+        list0.append(0)
+    for i in range(1,U):
+        list0.append(i)
+    #print(list0,"DecList")
+    return list0
+
+
+def Z(n):
+    Zlist = declist(n,1)
+    xlname = "Cayley Table for Z("+str(n)+").xlsx"
+    Zprinter(Zlist,n,xlname)
     
-    xlname = "Cayley Table for U("+str(U)+").xlsx"
-    "Runs the important defs"
-    Ulist = declist(U)
-    modfact = ModFactors(U)
+    
+def U(n):
+    Ulist = declist(n,0)
+    modfact = ModFactors(n)
     newU = Compare(Ulist,modfact)
-    tableprinter(newU,U,xlname)
+    xlname = "Cayley Table for U("+str(n)+").xlsx"
+    Uprinter(newU,n,xlname)
+    
+
+def custom(function,n,operation):
+    if(operation==0):#Multiplication
+        xlname = "Cayley Table under multiplication mod "+str(n)+" for "+str(function)+".xlsx"
+        Uprinter(function,n,xlname)
+        
+    if(operation==1):#Addition
+        xlname = "Cayley Table under addition mod "+str(n)+" for "+str(function)+".xlsx"
+        Zprinter(function,n,xlname)
+
+
+def main():
+    print("For Zn, use Z(n)")
+    print("For Un, use U(n)")
+    print("For a specific mod, use custom([],n,operation), where: ") 
+    print("[] is the subgroup you are using,")
+    print("n is the mod,")
+    print("and operation is either adding or multiplying. For addition, use 1. For multiplication, use 0")
+
+    
+    
